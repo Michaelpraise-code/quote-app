68186061022fea585ca57e0b1aa226eaee298b40
michaelpraise-code <okohmick24@gmail.com>
2025-04-02 12:03:25 +0100

    removed twitter button

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,11 +10,3 @@ async function getQuote() {
         console.log(error);
     }
 }
-
-function shareOnTwitter() {
-    const quoteText = document.getElementById("quote").innerText;
-    const authorText = document.getElementById("author").innerText;
-    const tweetUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(quoteText + " " + authorText)}`;
-    window.open(tweetUrl, "_blank");
-}
-getQuote();
