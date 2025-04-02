5f6b9f929297570eca4b2f57b3f6b0311fc5471c
michaelpraise-code <okohmick24@gmail.com>
2025-04-02 11:52:20 +0100

    added twitter button

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,11 @@ async function getQuote() {
         console.log(error);
     }
 }
+
+function shareOnTwitter() {
+    const quoteText = document.getElementById("quote").innerText;
+    const authorText = document.getElementById("author").innerText;
+    const tweetUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(quoteText + " " + authorText)}`;
+    window.open(tweetUrl, "_blank");
+}
+getQuote();
